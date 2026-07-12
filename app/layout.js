32de8446605edfed36e8f3fbd535350d8c8c5b74
Ibrahimbababov-create <Ibrahimbababov@gmail.com>
2026-07-12 14:27:49 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -8,7 +9,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+        {children}
+      </body>
     </html>
   );
 }
